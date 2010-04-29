d5f2129195dbf97b6c99bc24ffe458f986003b19
Ryan Davis <ryan@mokeys.org>
2010-04-28 23:36:03 -0400

    Fixed hex grid tiling.  Now the grid is based on three things:  1. a list of x/y pairs called the *unit-hex*  1. a hex-size-multiplier  1. the dimensions of the sdl window
    The *unit-hex* is the smallest specification of the polygon to make a hex, and is scaled up based upon hex-size-multiplier to get larger hexes.
    
    Tiling is then done across the sdl window.  This leaves some ragged edges.
    
    In retrospect, the *unit-hex* can probably be replaced by a single (width height) pair.

--- rpd-hexmap.lisp
+++ rpd-hexmap.lisp
@@ -5,43 +5,53 @@
 (defun make-map (width height)
   (make-array (list (* 2 width) (* 2 height))))
 
-(defvar *hex-surface* nil "sdl surface for the hexagon")
-(defun draw-map (map &key (hex-size-multiplier 3))
-  (let ((*hex-surface* (sdl:create-surface (* hex-size-multiplier 12)
-					   (* hex-size-multiplier 8))))
-    (sdl:draw-polygon (mapcar #'(lambda (x y) (sdl:point :x (* hex-size-multiplier x)
-							 :y (* hex-size-multiplier y)))
-			      '(0 4 8 12 8 4 0)
-			      '(4 8 8 4 0 0 4))
+(defvar *hex-surface* nil "sdl surface for a hexagon")
+(defvar *unit-hex* '((0 8) (6 16) (16 16) (22 8) (16 0) (6 0) (0 8)))
+(defun draw-map (map &key (hex-size-multiplier 1))
+  (let ((hex-width (1+ (* hex-size-multiplier
+			  (apply #'max (mapcar #'first *unit-hex*)))))
+	(hex-height (1+ (* hex-size-multiplier
+			   (apply #'max (mapcar #'second *unit-hex*))))))
+    (setf *hex-surface* (sdl:create-surface hex-width hex-height))
+    (sdl:draw-polygon (iter (for (x y) in *unit-hex*)
+			    (collect (sdl:point :x (* hex-size-multiplier x)
+						:y (* hex-size-multiplier y))))
 		      :surface *hex-surface*
 		      :color sdl:*white* )
-    (iter (for x from 0 to (sdl:width sdl:*default-display*) by (* hex-size-multiplier 8))
-	  (for rowx from 0)
-	  (iter (for y from 0 to (sdl:height sdl:*default-display*) by (* hex-size-multiplier 4))
-		(for rowy from 0)
-		(when (or (and (evenp rowx) (evenp rowy))
-			  (and (oddp rowx) (oddp rowy)))
-		  (sdl:draw-surface-at *hex-surface* (sdl:point :x x :y y)))
-		)
-	  ))
-  )
+    (iter (with y-tile-offset = (* hex-size-multiplier
+				   (second (first *unit-hex*))))
+	  (with x-tile-offset = (+ hex-width
+				   (* hex-size-multiplier
+				      (- (first (third *unit-hex*))
+					 (first (second *unit-hex*))))))
+	  (with x-odd-row-offset = (* hex-size-multiplier
+				      (first (third *unit-hex*))))
+	  (for y from 0 to (sdl:height sdl:*default-display*) by y-tile-offset)
+	  (for rowy from 0)
+	  (iter (for x from (if (evenp rowy) 0 x-odd-row-offset) to
+		     (sdl:width sdl:*default-display*) by x-tile-offset)
+		(sdl:draw-surface-at *hex-surface* (sdl:point :x x :y y))))))
 
 (defmacro with-hexmap ((map width height) &body body)
   `(let ((,map (make-map ,width ,height)))
-     ,@body))
+     (prog1
+	 (progn ,@body)
+       (when *hex-surface*
+	 (sdl:free *hex-surface*)
+	 (setf *hex-surface* nil)))))
 
 (defvar *map* nil)
 (defun test ()
-  (with-hexmap (*map* 20 20)
   (sdl:with-init ()
     (sdl:window 400 400)
-    (setf (sdl:frame-rate) 0)
+    (setf (sdl:frame-rate) 0)      
     (sdl:clear-display sdl:*black*)
-    (sdl:with-events ()
-      (:quit-event () t)
-      (:video-expose-event () (sdl:update-display))
-      (:idle ()
-	     (draw-map *map*)
-	     (sdl:update-display))))))
+    (with-hexmap (*map* 20 20)
+      (sdl:with-events ()
+	(:quit-event () t)
+	(:video-expose-event () (sdl:update-display))
+	(:idle ()
+	       (draw-map *map*)
+	       (sdl:update-display))))))
 
 
