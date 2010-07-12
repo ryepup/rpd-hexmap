c81007019489362950741c63eb47df5b266e6f59
Ryan Davis <ryan@mokeys.org>
2010-07-11 23:32:41 -0400

    made a 2D array as a datastructure that can be used for adjancency, etc, reworked drawing to use that to determine how much to draw.

--- rpd-hexmap.lisp
+++ rpd-hexmap.lisp
@@ -13,9 +13,12 @@
    (hex-surface :accessor hex-surface)
    (tiled-surface :accessor tiled-surface :initform nil)))
 
+(defvar *unit-hex* '((0 8) (6 16) (16 16) (22 8) (16 0) (6 0) (0 8)))
+
 (defmethod hex-width ((map hex-map))
   (* (sdl:x (hex-multiplier map))
      (apply #'max (mapcar #'first *unit-hex*))))
+
 (defmethod hex-height ((map hex-map))
   (* (sdl:y (hex-multiplier map))
      (apply #'max (mapcar #'second *unit-hex*))))
@@ -25,11 +28,24 @@
      (sdl:free ,thing)
      (setf ,thing nil)))
 
+
+
 (defmethod sdl:free ((map hex-map))
   (free-and-clear (hex-surface map))
   (free-and-clear (tiled-surface map)))
 
-(defvar *unit-hex* '((0 8) (6 16) (16 16) (22 8) (16 0) (6 0) (0 8)))
+;; #2A ((1 0 1 0 1 0)
+;;      (0 1 0 1 0 1)
+;;      (1 0 1 0 1 0))
+(defun %make-hex-grid (hexes-per-row hexes-per-column)
+  (let ((grid (make-array (list (* 2 hexes-per-column)
+				(* 2 hexes-per-row)))))
+    (dotimes (x (array-dimension grid 1))
+      (dotimes (y (array-dimension grid 0))
+	(when (or (and (evenp y) (evenp x))
+		  (and (oddp y) (oddp x)))
+	  (setf (aref grid y x) 1))))
+    grid))
 
 (defmethod make-tiled-surface ((map hex-map) &key width height)
   (with-accessors ((hsm hex-multiplier)
@@ -44,22 +60,19 @@
 						:y (* (sdl:y hsm) y))))
 		      :surface hex-surface
 		      :color sdl:*white* )
-    
-    (iter (with y-tile-offset = (* (sdl:y hsm) (second (first *unit-hex*))))
-	  (with x-tile-offset = (+ hex-width
-				   (* (sdl:x hsm)
-				      (- (first (third *unit-hex*))
-					 (first (second *unit-hex*))))))
-	  (with x-odd-row-offset = (* (sdl:x hsm) (first (third *unit-hex*))))
-	  (for y from 0 to (sdl:height tiled-surface) by y-tile-offset)
-	  (for rowy from 0  to (* 2 (1- (hexes-per-column map))))
-	  (iter (for x from (if (evenp rowy) 0 x-odd-row-offset) to
-		     (sdl:width tiled-surface) by x-tile-offset)
-		(for rowx from 0 to (- (hexes-per-row map) 1
-				       (if (evenp rowy) 0 1)))
-		(push (list x y) (hexes map))
-		(sdl:draw-surface-at-* hex-surface x y
-				       :surface tiled-surface)))))
+
+    (iter
+      (with grid = (%make-hex-grid (hexes-per-row map) (hexes-per-column map)))
+      (with y-tile-offset = (* (sdl:y hsm) (second (first *unit-hex*))))
+      (with x-tile-offset = (* (sdl:x hsm) (first (third *unit-hex*))))
+      (for rowy from 0 to (1- (array-dimension grid 0)))
+      (for y from 0 by y-tile-offset)
+      (iter (for rowx from 0 to (1- (array-dimension grid 1)))
+	    (for x from 0 by x-tile-offset)
+	    (when (plusp (aref grid rowy rowx))
+	      ;;save these coords in a grid on the map so we can do adjacency later
+	      (sdl:draw-surface-at-* hex-surface x y
+				     :surface tiled-surface))))))
 
 (defmethod draw-map ((map hex-map) &key (x 0) (y 0)
 		     (width (sdl:width sdl:*default-display*))
@@ -85,17 +98,12 @@
 (defun test ()
   (sdl:with-init ()
     (sdl:window 640 480)
-    (setf (sdl:frame-rate) 0)      
+    (setf (sdl:frame-rate) 20)      
     (sdl:clear-display sdl:*black*)
-    (with-hexmap (*map* 10 14)
+    (with-hexmap (*map* 5 7)
       (draw-map *map*)
       (sdl:with-events ()
-	(:quit-event () t)
-	(:MOUSE-MOTION-EVENT (:STATE STATE :X X :Y Y :X-REL X-REL :Y-REL Y-REL)
-			     (declare (ignore state x-rel y-rel))
-			     (draw-map *map*)
-			     (sdl:draw-pixel (sdl:point :x x :y y)
-					     :color sdl:*red*)) 
+	(:quit-event () t)   
 	(:video-expose-event () (sdl:update-display))
 	(:idle ()
 	       (sdl:update-display))))))
